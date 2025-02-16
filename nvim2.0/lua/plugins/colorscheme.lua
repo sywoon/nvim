71b94cd64206b2fd4e2f3875abca1d33f296163f
sywoon <sywoon@163.com>
2025-02-16 22:21:12 +0800

    自动启用colorscheme

--- nvim2.0/lua/plugins/colorscheme.lua
+++ nvim2.0/lua/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
+-- :colorscheme tokyonight-storm
 return {
     "folke/tokyonight.nvim",
+    opts = { --插件扩展配置
+        style = "storm",
+    },
+    config = function (_, opts)
+        require("tokyonight").setup(opts)
+		vim.cmd("colorscheme tokyonight")
+    end
 }
-
